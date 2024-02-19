3cbda0d4e59672c309a29fb525bd9fbb8aa3c201
Glitch (glitch-hello-website) <none>
2024-02-19 19:20:42 +0000

    🕰🏝 Checkpoint ./index.js:61388766/469

--- index.js
+++ index.js
@@ -8,26 +8,30 @@ canvas.height = window.innerHeight;
   console.log('Window inner width:', window.innerWidth);
     console.log('Window inner height:', window.innerHeight);
 
-    let time = 0.5;
-    const characters = "is a week-long documentation and is a reflection on hyperconsumerism in the digital age and visualizes our loss of time. It explores both passive and active notifications visually. This website mimics how digital notifications interrupt our lives and influence our behavior. The timing of each ad reflects real-life documentation.";
-    let frequency = 0.6;
+    let time = 0.05;
+    const paragraph = "is a week-long documentation and is a reflection on hyperconsumerism in the digital age and visualizes our loss of time. It explores both passive and active notifications visually. This website mimics how digital notifications interrupt our lives and influence our behavior. The timing of each ad reflects real-life documentation.";
+    let frequency = 100;
     let amplitude = 1400;
+  
+  const fontSize = 24;
+    const fontFamily = 'GTAlpinaTypewriter';
+    const textColor = 'blue';
 
     function drawControlledSineWave() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        const depth = 800; // Depth of the 3D effect
-        const fontSize = 42;
-        ctx.font = `${fontSize}px`;
+        const depth = 400; // Depth of the 3D effect
+        ctx.font = `${fontSize}px ${fontFamily}`;
+        ctx.fillStyle = textColor;
 
         for (let x = 0; x < canvas.width; x += fontSize) {
             const y = amplitude * Math.sin(frequency * x + time) + canvas.height / 2;
             const z = depth * Math.sin(frequency * x + time); // Add depth variation
 
-            const characterIndex = Math.floor((y - z) / amplitude * characters.length);
-            const character = characters.charAt(characterIndex);
+            const paragraphIndex = Math.floor((y - z) / amplitude * paragraphs.length);
+            const paragraphs = paragraph.charAt(paragraphIndex);
 
-            ctx.fillText(character, x, y);
+            ctx.fillText(paragraph, x, y);
         }
 
         time += 0.02; // Adjust the speed of the animation
