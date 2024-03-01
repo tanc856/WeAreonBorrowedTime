b381ba09407f077d111f85960b943ad3ca8fa82b
Glitch (glitch-hello-website) <none>
2024-03-01 03:27:40 +0000

    🛋🌁 Checkpoint ./index.html:61388766/11556 ./index.js:61388766/2503

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const canvas = document.getElementById('BackgroundCanvas');
-    const ctx = canvas.getContext('2d');
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    let time = 0;
-    const paragraph = "is a week-long documentation and is a reflection on hyperconsumerism in the digital age and visualizes our loss of time. It explores both passive and active notifications visually. This website mimics how digital notifications interrupt our lives and influence our behavior. The timing of each ad reflects real-life documentation.";
-
-    const fontSize = 24;
-    const fontFamily = '"plantin", sans-serif';
-    const textColor = 'blue';
-
-    function drawParagraph() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        ctx.font = `${fontSize}px ${fontFamily}`;
-        ctx.fillStyle = textColor;
-      
-      let x = canvas.width - (time % (canvas.width + ctx.measureText(paragraph).width));
-        let y = fontSize;
-      
-      // Draw the paragraph line by line
-        let linesDrawn = 0;
-        while (y < canvas.height) {
-            ctx.fillText(paragraph, x, y);
-            y += fontSize * 1.5; // Adjust line spacing as needed
-            linesDrawn++;
+ function toggleDescription() {
+        var description = document.getElementById("description");
+        if (description.style.display === "none") {
+          description.style.display = "block";
+          document.querySelector('.description-toggle').textContent = "-";
+        } else {
+          description.style.display = "none";
+          document.querySelector('.description-toggle').textContent = "+";
         }
-
-        // Increment time for continuous animation
-        time += 1; // Adjust the speed of the animation
-
-        // Reset time and clear the canvas when all lines are drawn
-        if (linesDrawn * (fontSize * 1.5) >= canvas.height) {
-            time = 0;
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-        }
-    }
-  
-  // Initial drawing
-    drawParagraph();
-
-    // Start default animation
-    setInterval(drawParagraph, 30); // Adjust the interval as needed
-
-});
+      }
