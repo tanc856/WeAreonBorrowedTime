f507cb8756bb184e3a804b046e0de91e6380ae34
Glitch (glitch-hello-website) <none>
2024-03-17 01:59:13 +0000

    🚌🚏 Checkpoint ./index.html:61388766/7703 ./index.js:61388766/990

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 document.addEventListener('mousemove', function(e) {
     const container = document.querySelector('.background-container');
-    const mouseX = e.pageX;
-    const mouseY = e.pageY;
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
     const containerRect = container.getBoundingClientRect();
-    const backgroundX = ((mouseX - containerRect.left) / containerRect.width) * 500;
+    const backgroundX = ((mouseX - containerRect.left) / containerRect.width) * 100;
     const backgroundY = ((mouseY - containerRect.top) / containerRect.height) * 100;
     container.style.backgroundPosition = `${backgroundX}% ${backgroundY}%`;
 });
