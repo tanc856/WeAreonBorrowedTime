d9b317ac0a1b89a9c653e00a8ed83ebc81fdb2ae
Glitch (glitch-hello-website) <none>
2024-03-17 02:44:55 +0000

    🚘💅 Checkpoint ./index.html:61388766/1431 ./index.js:61388766/1138

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ document.addEventListener('mousemove', function(e) {
     const backgroundY = ((mouseY - containerRect.top) / containerRect.height) * 100;
     container.style.backgroundPosition = `${backgroundX}% ${backgroundY}%`;
 });
+
+function toggleElements() {
+        const windowWidth = window.innerWidth;
+        const cursorX = event.clientX;
+        const threshold = 400; // Adjust this value as needed
+
+        // Check if cursor is on the left side of the canvas
+        if (cursorX <= threshold) {
+            document.querySelector('.passive').style.display = 'block';
+            document.querySelector('.web').style.display = 'none';
+        } else {
+            document.querySelector('.passive').style.display = 'none';
+            document.querySelector('.web').style.display = 'none';
+        }
+
+        // Check if cursor is on the right side of the canvas
+        if (cursorX >= (windowWidth - threshold)) {
+            document.querySelector('.active').style.display = 'block';
+        } else {
+            document.querySelector('.active').style.display = 'none';
+            document.querySelector('.web').style.display = 'none';
+        }
+    }
+
+    // Event listener for mousemove event to track cursor position
+    document.addEventListener('mousemove', toggleElements);
