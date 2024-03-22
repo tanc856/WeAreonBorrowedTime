c0af1891f6c15032318b49420a323a2ede8ab785
Glitch (glitch-hello-website) <none>
2024-03-22 02:31:29 +0000

    🍤👩‍👩‍👦 Checkpoint ./active-script.js:61388766/9634 ./active-style.css:61388766/595

--- active-script.js
+++ active-script.js
@@ -18,21 +18,15 @@ function replaceWords() {
     if (Math.random() < 0.2) {
       // Adjust the probability as needed
       // Replace a random word with the replacement words
-const randomIndex = Math.floor(Math.random() * words.length);
-const randomWord =
-    replacementWords[Math.floor(Math.random() * replacementWords.length)];
-words[randomIndex] = `<span class="highlight">${randomWord}</span>`;
+      const randomIndex = Math.floor(Math.random() * words.length);
+      const randomWord =
+        replacementWords[Math.floor(Math.random() * replacementWords.length)];
+      words[randomIndex] = `<span class="highlight">${randomWord}</span>`;
 
-// Update the column's content
-column.innerHTML = words.join(" ");
-
-// Restore original words after a short delay
-setTimeout(() => {
-    // Remove the added HTML span tags to prevent duplication
-    words[randomIndex] = randomWord;
-    column.innerHTML = words.join(" ");
-}, 500); // Adjust the delay time as needed
-
+      // Update the column's content
+      column.innerHTML = words.join(" ");
+    }
+  });
 
   // Call the function recursively for continuous word randomization
   setTimeout(replaceWords, 1000); // Adjust the interval as needed, the bigger the slower
